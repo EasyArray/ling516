79d828eca48635e401689f6e08547db1f63447fd
Ezra Keshet <ezrakeshet@gmail.com>
2025-02-13 16:46:57 +0000

    simplify lambda calls

--- phosphorus/__init__.py
+++ phosphorus/__init__.py
@@ -28,8 +28,21 @@ ip_asts.append(ExprTransformer())
 # values provided by a context.
 
 class VariableReplacer(NodeTransformer):
-  def __init__(self, context): self.context = context
+  def __init__(self, context): 
+    self.context = context
 
+  def visit_Call(self, node):
+    if isinstance(node.func, Lambda):
+      vars = {arg.arg for arg in node.func.args.args}
+      for arg in node.args:
+        self.visit(arg)
+      context = self.context | dict(zip(vars, node.args))
+      #print('new context', context)
+      return VariableReplacer(context).visit(node.func.body)
+    
+    self.generic_visit(node)
+    return node
+  
   def visit_Lambda(self, node):
     args = {arg.arg for arg in node.args.args}
     shadowed = {v:self.context.pop(v) for v in args if v in self.context}
@@ -40,6 +53,8 @@ class VariableReplacer(NodeTransformer):
   def visit_Name(self, node):
     if node.id in self.context:
       #print(f'Replacing {node.id} with {self.context[node.id]}, {type(self.context[node.id])}')
+      if isinstance(self.context[node.id], AST):
+        return self.context[node.id]
       if hasattr(self.context[node.id], 'to_ast'):
         return self.context[node.id].to_ast()
       return Constant(value=str(self.context[node.id]))
@@ -103,10 +118,11 @@ class Function(SemVal):
         raise ValueError(msg)
 
   def __call__(self, *args):
+    # NOTE TO SELF: some logic is replicated in VariableReplacer
     node = parse(self.value, mode='eval').body
-    if context := dict(zip(self.vars, args)):
-      node = VariableReplacer(context).visit(node)
-      #print(dump(node))
+    context = dict(zip(self.vars, args))
+    node = VariableReplacer(context).visit(node)
+    #print(dump(node))
 
     out_type = self.type.output()
     if isinstance(node, Lambda):
